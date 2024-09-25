49a0efadf09547933656303c608189a6e78ed34b
Peter <youngeunpc@yahoo.com>
2024-09-25 16:29:48 -0300

    changes

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger").addEventListener("click", function() {
+    const dropdown = document.querySelector(".dropdown");
+    dropdown.classList.toggle("active"); // Toggle active class on dropdown
+});
