677787d008e7c6f606849a2bff11b4111968e9da
Peter <youngeunpc@yahoo.com>
2024-09-27 20:21:53 -0300

    changes

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 const hamburger = document.getElementById("hamburger");
 const dropdown = document.querySelector(".dropdown");
+const closeIcon = document.getElementById("close-icon");
 
 function toggleMenu() {
     dropdown.classList.toggle("active");
-    hamburger.classList.toggle("active");
+    hamburger.classList.toggle("hidden");
+    closeIcon.classList.toggle("hidden");
 }
 
-document.getElementById("hamburger").addEventListener("click", toggleMenu);
+function toggleCloseIcon() {
+    dropdown.classList.toggle("active");
+    hamburger.classList.toggle("hidden");
+    closeIcon.classList.toggle("hidden");
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+closeIcon.addEventListener("click", toggleCloseIcon);
 
 document.addEventListener("click", function(event) {
     if (!hamburger.contains(event.target) && !dropdown.contains(event.target)) {
         dropdown.classList.remove("active");
-        hamburger.classList.remove("active");
+        hamburger.classList.remove("hidden");
+        closeIcon.classList.add("hidden");
     }
 });
 
 document.addEventListener("keydown", function(event) {
     if (event.key === "Escape") {
         dropdown.classList.remove("active");
-        hamburger.classList.remove("active");
+        hamburger.classList.remove("hidden");
+        closeIcon.classList.add("hidden");
     }
 });
